2cae5a4b10e1bf6d2bc3d3ccea4547ab9fa3ae00
Fernanda Hamada <fernandakubayashi@gmail.com>
2019-12-29 14:50:52 -0300

    Adding Model class for Theacher

---
+++ CustomValidation/SkillsValidate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudWEF2.CustomValidation
+{
+    public class SkillsValidate : Attribute, IModelValidatior
+    {
+        public string[] Allowed { get; set; }
+        public string ErrorMessage { get; set; }
+        public IEnumerable<ModelValidationResult> Validate(ModelValidateContext context)
+        {
+            if (Allowed.Contains(context.Model as string))
+                return Enumerable.Empty<ModelValidationResult>();
+            else
+                return new List<ModelValidationResult> {
+                new ModelValidationResult("", ErrorMessage)};
+        }
+
+    }
+}
---
+++ Models/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using CrudWEF2.CustomValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudWEF2.Models
+{
+    public class Teacher
+    {
+        [Required]
+        public int Id { get; set; } [Required]
+        public string Name { get; set; } [Required]
+        [SkillsValidate(Allowed = new string[] { "ASP.NET Core", "ASP.NET MVC", "ASP.NET Web Forms" }, ErrorMessage = "You skills are invalid")]
+        public string Skills { get; set; } [Range(5, 50)]
+        public int TotalStudents { get; set; } [Required]
+        public decimal Salary { get; set; }
+
+        [Required]
+        public DateTime AddeOn { get; set; }
+
+    }
+
+}
